19624e91eb22db5caa05d018fe91fdb26a47d0e8
ankitdebnath003 <ankit.debnath@innoraft.com>
2023-06-25 14:25:08 +0530

    FT2023-305: Created a new custom module and implement existing and custom hook and completed the task of Hook.

---
+++ web/modules/custom/hook_example/hook_example.api.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Structure of a custom hook.
+ */
+
+/**
+ * Respond to node view count being incremented.
+ *
+ * This hooks allows modules to respond whenever the total number of times the
+ * current user has viewed a specific node during their current session is
+ * increased.
+ *
+ * @param int $current_count
+ *   The number of times that the current user has viewed the node during this
+ *   session.
+ * @param EntityInterface $node
+ *   The node being viewed.
+ */
+function hook_count_incremented(int $current_count, object $entity) {
+  // Further implementation here.
+}
+
+?>
