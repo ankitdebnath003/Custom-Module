dd75791cf1daa60f83168a567f3d54b29b752221
ankitdebnath003 <ankit.debnath@innoraft.com>
2023-07-14 11:14:27 +0530

    FT2023-303: Fixing all the coding standards according to PHPCS.

--- web/modules/custom/welcome/src/Controller/WelcomeController.php
+++ web/modules/custom/welcome/src/Controller/WelcomeController.php
@@ -2,34 +2,54 @@
 
 namespace Drupal\welcome\Controller;
 
-use Drupal\Core\Cache\Cache;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * This class is used to show the user the hello message with their names.
- * 
+ *
  * @package Drupal\welcome\Controller
  */
-class WelcomeController extends ControllerBase 
-{
+class WelcomeController extends ControllerBase {
 
   /**
-   * This function is used to show the current user a hello message with their name.
+   * Stores the metadata of Account Interface.
+   *
+   * @var Drupal\Core\Session\AccountInterface
+   */
+  private $account;
+
+  /**
+   * Initialize the object of AccountInterface to the class variable.
+   *
+   * @param Drupal\Core\Session\AccountInterface $account
+   *   Stores the account information of the currently logged in user.
+   */
+  public function __construct(AccountInterface $account) {
+    $this->account = $account;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * This function is used to show the current user a hello message with name.
    *
    * @return array
    *   Returns the hello message with the current user's name.
    */
   public function welcomeCurrentUser() {
-    $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
-    $user_name = \Drupal::currentUser()->getAccountName();
+    $user_name = $this->account->getAccountName();
     return [
       '#title' => 'Hello ' . ucfirst($user_name),
-      '#cache' => [
-        'tags' => $user->getCacheTags(),
-      ]
     ];
   }
+
 }
-
-
-?>
