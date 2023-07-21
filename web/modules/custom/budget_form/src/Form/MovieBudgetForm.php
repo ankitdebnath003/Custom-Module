2c6ec7615711a292067066ff55097fbee0417600
ankitdebnath003 <ankit.debnath@innoraft.com>
2023-07-21 22:19:53 +0530

    FT2023-328: Create a module for menu and add a custom menu to the administrator menu which redirects the user to movie budget form to set budget and when view a movie_entity type node then show the message accordingly.

---
+++ web/modules/custom/budget_form/src/Form/MovieBudgetForm.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\budget_form\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Stores the movie budget from the config form.
+ */
+class MovieBudgetForm extends ConfigFormBase {
+
+  /**
+   * Stores the config form name.
+   *
+   * @var string
+   */
+  const CONFIG = 'movie_budget_form.settings';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return MovieBudgetForm::CONFIG;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [MovieBudgetForm::CONFIG];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config(MovieBudgetForm::CONFIG);
+    $form['budget'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Example'),
+      '#default_value' => $config->get('budget'),
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config(MovieBudgetForm::CONFIG)
+      ->set('budget', $form_state->getValue('budget'))
+      ->save();
+  }
+
+}
